4a70b520a117b9d6296c4d42f1458ef987a3c005
Mrinmoy Sarkar <mrin2ksarkar@gmail.com>
2023-04-19 01:23:09 +0530

    code cleaning/bug fixes

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
-import os
-from selenium.webdriver.chrome.webdriver import WebDriver
-from noaa_sdk import NOAA # NOAA-SDK python
-
-# Global Variables
-n = NOAA()
-link = 'https://www.google.com'
-pallin_num = 112211
-non_pallin_num = 12345
-c = 0
+from noaa_sdk import NOAA  # NOAA-SDK python
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 
 # Test Functions
-def check_pallindrome(num):
-    num = str(num)
-    num_copy = num
-    num = num[::-1]
-    if num_copy != num:
-        return True
-    return False
+
+
+def check_palindrome(num):
+    return str(num) == str(num)[::-1]
+
 
 def check_webpage(link: str) -> None:
     # Initialize Chrome WebDriver
-    driver: WebDriver = WebDriver()
+    driver: Chrome = Chrome()
     # Navigate to webpage
     driver.get(link)
 
-    # # Create directory to save the file
-    # path: str = os.getcwd()
-    # parent_path: str = os.path.abspath(os.path.join(path, os.pardir))
-    # out_dir_path: str = os.path.join(parent_path, "HTML_output")
-    # os.makedirs(out_dir_path)
-    
     # Wait for page to load and all elements to become visible
     driver.implicitly_wait(10)
     # Get all visible elements on the page
-    visible_elements = driver.find_elements_by_css_selector("*:not([style*=‘display:none’]):not([style*=‘display: none’])")
+    visible_elements = driver.find_elements(
+        By.CSS_SELECTOR, "*[style]:not([style*='display:none']):not([style*='display: none'])")
     # Save visible elements to an HTML file
     with open("visible_elements.html", "w", encoding="utf-8") as file:
         file.write("<html><body>")
@@ -43,39 +29,49 @@ def check_webpage(link: str) -> None:
     # Close the WebDriver
     driver.quit()
 
-
 # Using the NOAA-SDK in python
+
+
 def check_weather():
+    n = NOAA()
     lat = 40.7314
     lon = -73.8656
-    forecasts = n.get_forecasts(coordinates=(lat, lon))
+    try:
+        forecasts = n.get_forecasts(coordinates=(lat, lon))
+    except Exception as err:
+        print("Error occurred while checking weather:", err)
 
 
 if __name__ == "__main__":
+    link = 'https://www.google.com'
+    pallin_num = 112211
+    non_pallin_num = 12345
+    tests_passed = 0
 
-    try: # Check the pallindrome function
-        if check_pallindrome(pallin_num) == False or check_pallindrome(non_pallin_num) == True:
+    try:  # Check the pallindrome function
+        if not check_palindrome(pallin_num) or check_palindrome(non_pallin_num):
             raise Exception
         else:
             print("Test 1 passed.")
-            c+=1
+            tests_passed += 1
     except Exception as err:
         print("Test case 1 failed. Please correct the pallindrome function")
         print(err)
 
-    try: # Check the weather function
+    try:  # Check the weather function
         check_weather()
-        c+=1
+        tests_passed += 1
         print("Test case 2 passed.")
     except Exception as err:
         print("Test case 2 failed.")
         print(err)
 
-    try: # Check the webpage function
+    try:  # Check the webpage function
         check_webpage(link)
-        c+=1
+        tests_passed += 1
         print("Test case 3 passed.")
     except Exception as err:
         print("Test case 3 failed.")
         print(err)
-    print(f"Total {c}/3 tests passed")
+
+    print(f"Total {tests_passed}/3 tests passed")
